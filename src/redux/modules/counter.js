783c808cd33e78350ad0be1d1b875fc051de232f
Eunji Roh <nonjee888@gmail.com>
2022-08-24 17:15:06 +0900

    리액트 숙련 학습자료

---
+++ src/redux/modules/counter.js
@@ -0,0 +1,43 @@
+//초기 상태값
+const initialState = {
+    number: 0,
+};
+
+//액션밸류
+const ADD_NUMBER = "ADD_NUMBER";
+const MINUS_NUMBER = "MINUS_NUMBER";
+
+//액션크리에이터
+export const addNumber = (payload) => {
+    return {
+        type: ADD_NUMBER,
+        payload,
+    };
+};
+
+export const minusNumber = (payload) => {
+    return {
+        type: MINUS_NUMBER,
+        payload,
+    };
+};
+
+//리듀서
+const counter = (state = initialState, action) => {
+    // console.log(state)
+    switch (action.type) {
+        case ADD_NUMBER:
+            return{
+                number: state.number + action.payload,
+            }
+
+        case MINUS_NUMBER:
+            return{
+                number: state.number - action.payload,
+            }
+        default:
+            return state;
+    }
+};
+
+export default counter;
